17f44c4e2584bd2965e7dbdec07712433dfe9052
liggia <liggia_elena@usp.br>
2021-08-01 20:45:21 -0300

    Quase finalizado, falta embaralhar direito

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 let listaGifs = [
-    "fiestaparrot.gif",
-    "bobrossparrot.gif",
-    "explodyparrot.gif",
-    "metalparrot.gif",
-    "revertitparrot.gif",
-    "tripletsparrot.gif",
-    "unicornparrot.gif",
-    "fiestaparrot.gif",
-    "bobrossparrot.gif",
-    "explodyparrot.gif",
-    "metalparrot.gif",
-    "revertitparrot.gif",
-    "tripletsparrot.gif",
-    "unicornparrot.gif"
-]
+    "fiestaparrot",
+    "bobrossparrot",
+    "explodyparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot",
+];
+let paresDosGifs = [];
+//listaGifs.sort(comparador);
 let quantidade = 0;
+let contador = 0;
 perguntarQuantidade();
 
 function perguntarQuantidade(){
@@ -31,18 +27,70 @@ function perguntarQuantidade(){
 function mostrarCartas(quantidade){
     let jogo = document.querySelector(".jogo");
     let quantidadeCartas = "";
+    
     listaGifs.sort(comparador);
-
-    for(let i=0; i!= quantidade; i++){
-        quantidadeCartas += `<div class="cartas " onclick="virarCarta(this)"><img src="midia/front.png"/><img src="midia/${listaGifs[i]}" class="gif"/></div>`;
+    for(let i=0; i<quantidade; i++){
+        paresDosGifs.push(`${listaGifs[i]}`);
     }
+
+    for(let j=0; j!= quantidade/2; j++){
+        quantidadeCartas += `<div class="cartas " onclick="virarCarta(this)">
+        <img src="midia/front.png"/><img src="midia/${listaGifs[j]}.gif" class="gif ${listaGifs[j]}" />
+        </div>`;     
+    }
+     paresDosGifs.sort(comparador);
+     for(let j=0; j!= quantidade/2; j++){
+        quantidadeCartas += `<div class="cartas " onclick="virarCarta(this)">
+        <img src="midia/front.png"/><img src="midia/${paresDosGifs[j]}.gif" class="gif ${paresDosGifs[j]}" />
+        </div>`;     
+    }
+
     jogo.innerHTML = quantidadeCartas;
+    alert(paresDosGifs)
 }
 
 function comparador() { 
-	return Math.random() - 0.5; 
-} 
+            return Math.random() - 0.5; 
+            } 
 
 function virarCarta(cartaSelecionada){
-    cartaSelecionada.classList.add("permanecer-virada");
+   let cartasAbertas = document.querySelector(".virei");
+   let gifCartaSelecionada = cartaSelecionada.querySelector(".gif");
+   contador++;
+     
+   
+
+    if(cartasAbertas === null){
+    cartaSelecionada.classList.add("virei");
+    }
+    else{
+        cartaSelecionada.classList.add("virei");
+        let gifCartaAberta = cartasAbertas.querySelector(".gif");
+            
+        if( gifCartaSelecionada.className === gifCartaAberta.className){
+        
+             cartaSelecionada.classList.add("permanecer-virada");
+             cartasAbertas.classList.add("permanecer-virada");
+             cartaSelecionada.classList.remove("virei");
+             cartasAbertas.classList.remove("virei");
+             
+            finalDoJogo();
+        }
+        else{
+               setTimeout(function (){
+                   cartasAbertas.classList.remove("virei");
+                cartaSelecionada.classList.remove("virei");
+               }, 1000);      
+        }
+    }
+   
+    
+}
+
+function finalDoJogo(){
+    let cartasGanhas = document.querySelectorAll(".permanecer-virada");
+    if (cartasGanhas.length === quantidade){
+        setTimeout(alert , 1000, `Você ganhou em ${contador} jogadas!` );
+       
+    }
 }
